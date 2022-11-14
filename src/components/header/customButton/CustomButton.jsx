ba24d96d51aab076c1b22cb4029b2d791b1ddb69
jsonmullings <wjasonmullings@gmail.com>
2022-11-13 19:25:35 -0500

    Basic set up and hero/header section finished.

---
+++ src/components/header/customButton/CustomButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './custom-button.styles.scss';
+
+
+function CustomButton() {
+  return (
+    <div className='btn btn-white btn-animated'>discover our tours</div>
+  )
+}
+
+export default CustomButton
